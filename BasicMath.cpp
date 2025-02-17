9f037bb7e30047daa8f74443785e74cbba4408f1
Xiaohan Tian <xiaohan.tian@stu.ocsb.ca>
2025-02-17 05:32:17 +0000

    Added BasicMath.cpp

---
+++ BasicMath.cpp
@@ -0,0 +1,17 @@
+// Copyright 2025 Xiaohan Tian. All rights reserved.
+// Created by Xiaohan Tian
+// Created on June 2020
+// This program does basic math
+
+#include <cmath>
+#include <iostream>
+
+    int main() {
+    std::cout << "9+2=" << (9 + 2) << std::endl;
+    std::cout << "7-1=" << (7 - 1) << std::endl;
+    std::cout << "4/2=" << (4 / 2) << std::endl;
+    std::cout << "4+4/2=" << (4 + 4 / 2) << std::endl;
+    std::cout << "3+4*2=" << (3 + 4 * 2) << std::endl;
+    std::cout << "5+2^3=" << (5 + 2 ^ 3) << std::endl;
+    std::cout << "5+pow(2,3)=" << (5 + pow(2, 3)) << std::endl;
+}
